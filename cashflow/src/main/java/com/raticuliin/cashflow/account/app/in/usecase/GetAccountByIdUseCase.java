7e9302620e007d4341940f7b0bbcdd6365d17a30
Raticuliin <alvarezlamasivanroque@gmail.com>
2024-09-24 12:52:17 +0200

    .intellij delete

---
+++ cashflow/src/main/java/com/raticuliin/cashflow/account/app/in/usecase/GetAccountByIdUseCase.java
@@ -0,0 +1,9 @@
+package com.raticuliin.cashflow.account.app.in.usecase;
+
+import com.raticuliin.cashflow.account.domain.Account;
+
+public interface GetAccountByIdUseCase {
+
+    Account getAccountById(int id);
+
+}
